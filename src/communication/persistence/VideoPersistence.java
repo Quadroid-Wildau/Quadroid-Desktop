cd02187444b5ba774673ce74b7dd8da1c86cf2ec
Georg Baumgarten <georg@wonderweblabs.io>
2014-01-09 11:34:30 +0100

    Fixed bug where video recording under linux wasn't possible

--- src/communication/persistence/VideoPersistence.java
+++ src/communication/persistence/VideoPersistence.java
@@ -7,15 +7,15 @@ import com.googlecode.javacv.cpp.opencv_highgui.CvCapture;
 
 public class VideoPersistence {
 	
-	private static final int BITRATE = 20 * 1024 * 1024; //20 Mbit/s
+	private static final int BITRATE = 5 * 1024 * 1024; //5 Mbit/s
 
 	public FFmpegFrameRecorder createRecorder(CvCapture capture, String filepath) throws Exception {
 		int width = (int) opencv_highgui.cvGetCaptureProperty(capture, opencv_highgui.CV_CAP_PROP_FRAME_WIDTH);
 		int height = (int) opencv_highgui.cvGetCaptureProperty(capture, opencv_highgui.CV_CAP_PROP_FRAME_HEIGHT);
-		double fps = opencv_highgui.cvGetCaptureProperty(capture, opencv_highgui.CV_CAP_PROP_FPS);
+//		double fps = opencv_highgui.cvGetCaptureProperty(capture, opencv_highgui.CV_CAP_PROP_FPS);
 		
 		FFmpegFrameRecorder mFrameRecorder = FFmpegFrameRecorder.createDefault(filepath, width, height);
-		mFrameRecorder.setFrameRate(fps);
+//		mFrameRecorder.setFrameRate(fps);
 		
 		mFrameRecorder.setVideoBitrate(BITRATE);
 		
